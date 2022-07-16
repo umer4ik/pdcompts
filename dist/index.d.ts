1e439cb55286533df0d397cf42c6f61fb35d460d
Umer Hurramov <umer.hurramov@gmail.com>
2022-07-16 20:36:38 +0300

    Uncomment dist to handle css issue

---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+declare type ButtonProps = {
+    label: React.ReactNode;
+};
+declare function Button({ label }: ButtonProps): JSX.Element;
+
+export { Button };
---
+++ dist/index.mjs
@@ -0,0 +1,19 @@
+// src/components/Button/Button.tsx
+import * as React from "react";
+
+// src/css-utils.ts
+var prefix = "pdcompt-";
+function cls(className) {
+  return `${prefix}${className}`;
+}
+
+// src/components/Button/Button.tsx
+function Button({ label }) {
+  return /* @__PURE__ */ React.createElement("button", {
+    className: cls("button")
+  }, label);
+}
+export {
+  Button
+};
+//# sourceMappingURL=index.mjs.map
